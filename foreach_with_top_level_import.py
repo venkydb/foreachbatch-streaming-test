f0120d1230049f00c8224a523e1634299ef090e9
Corbin-A <Corbin-A>
2024-06-11 08:23:31 +0000

    initial commit

---
+++ foreach_with_top_level_import.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+from shared_functions import shared_foo
+
+# Define processing function
+def process_batch(batch_df, batch_id):
+    shared_foo()
+
+df_stream = spark.readStream.table("samples.nyctaxi.trips")
+write_stream = df_stream.writeStream.foreachBatch(process_batch)
+query = write_stream.trigger(once=True).start()
+query.awaitTermination()
+
+# COMMAND ----------
+
+
