34340850207183963a59b65e6f5b7da25745bddd
venkydb <77368268+venkydb@users.noreply.github.com>
2024-06-11 10:19:33 -0700

    Update foreach_with_top_level_import.py
    changing to foreachbatch

--- foreach_with_top_level_import.py
+++ foreach_with_top_level_import.py
@@ -6,7 +6,7 @@ def process_record(record):
     shared_foo()
 
 df_stream = spark.readStream.table("samples.nyctaxi.trips")
-write_stream = df_stream.writeStream.foreach(process_record)
+write_stream = df_stream.writeStream.foreachBatch(process_record)
 query = write_stream.trigger(once=True).start()
 query.awaitTermination()
 
