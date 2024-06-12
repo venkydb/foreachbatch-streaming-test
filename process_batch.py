de348fc4115d40e759624c15f4cae73bba81ae42
venkydb <77368268+venkydb@users.noreply.github.com>
2024-06-11 17:29:21 -0700

    Delete process_batch.py

--- process_batch.py
+++
@@ -1,9 +0,0 @@
-# Define processing function
-def process_record(batch_df, batch_id):
-  from shared_functions import shared_foo
-  shared_foo()
-
-def process_batch_func(df_stream):
-  write_stream = df_stream.writeStream.foreachBatch(process_record)
-  query = write_stream.trigger(once=True).start()
-  query.awaitTermination()
